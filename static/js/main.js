1e233939c29313bb2a0bcb68c6916efbeb80fa57
sean-david-welch <seanwelch51199@icloud.com>
2022-11-20 17:35:29 +0000

    deleted bootstrap and created custom templates & stylesheets

---
+++ static/js/main.js
@@ -0,0 +1,34 @@
+// const navbar = document.getElementById('navbar');
+// let scrolled = false;
+
+// window.onscroll = function() {
+//     if (window.scrollY > 550) {
+//         navbar.classList.remove('top');
+//         if (!scrolled) {
+//             navbar.style.transform = 'translateY(-10px)';
+//         }
+//         setTimeout(function() {
+//             navbar.style.transform = 'translateY(0)';
+//             scrolled = true;
+//         }, 300)
+//     } else {
+//         navbar.classList.add('top');
+//         scrolled = false;
+//     }
+// }
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function (e) {
+    if (this.hash !== '') {
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 60,
+            },
+            1000
+        );
+    }
+});
