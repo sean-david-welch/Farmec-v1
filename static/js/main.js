d2d84aedb7303c9f33d862a11207d14134e0c6e3
sean-david-welch <seanwelch51199@icloud.com>
2022-11-24 18:14:25 +0000

    styles updated

--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,11 @@
-// const navbar = document.getElementById('navbar');
-// let scrolled = false;
+// To top button
+const toTop = document.querySelector("to-top");
 
-// window.onscroll = function() {
-//     if (window.scrollY > 550) {
-//         navbar.classList.remove('top');
-//         if (!scrolled) {
-//             navbar.style.transform = 'translateY(-10px)';
-//         }
-//         setTimeout(function() {
-//             navbar.style.transform = 'translateY(0)';
-//             scrolled = true;
-//         }, 300)
-//     } else {
-//         navbar.classList.add('top');
-//         scrolled = false;
-//     }
-// }
+window.addEventListener("scroll", () => {
+    if (window.scrollY < 100) {
+        toTop.classList.remove("active");
+    }
+})
 
 // Smooth Scrolling
 $('#navbar a, .btn').on('click', function (e) {
@@ -26,18 +16,9 @@ $('#navbar a, .btn').on('click', function (e) {
 
         $('html, body').animate(
             {
-                scrollTop: $(hash).offset().top - 60,
+                scrollTop: $(hash).offset().top - 100,
             },
-            500
+            1000
         );
     }
 });
-
-// To top button
-const toTop = document.querySelector("to-top");
-
-window.addEventListener("scroll", () => {
-    if (window.scrollY < 100) {
-        toTop.classList.remove("active");
-    }
-})
