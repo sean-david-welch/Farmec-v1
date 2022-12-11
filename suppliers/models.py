f324033235729eea629c225448679168a247a2ba
sean-david-welch <seanwelch51199@icloud.com>
2022-12-11 13:28:45 +0000

    added some supplier page markup

--- suppliers/models.py
+++ suppliers/models.py
@@ -7,7 +7,7 @@ class Supplier(models.Model):
     logo_image = models.ImageField(null=True, blank=True, upload_to='models/', default="models/default.jpg")
     marketing_image = models.ImageField(null=True, blank=True, upload_to='models/', default="models/default.jpg")
     description = models.TextField(blank=True, null=True)
-    short_description = models.TextField(max_length=200, blank=True, null=True)
+    short_description = models.TextField(blank=True, null=True)
     social_facebook = models.CharField(max_length=200, blank=True, null=True)
     social_twitter = models.CharField(max_length=200, blank=True, null=True)
     social_instagram = models.CharField(max_length=200, blank=True, null=True)
@@ -39,6 +39,7 @@ class Supplier(models.Model):
         except: 
             url = ''
         return url
+
 
 class Fact(models.Model):
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, null=True, blank=True)
