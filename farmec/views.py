f9d0d1eb5ad999725b97e07ed51b148104d1eb11
sean-david-welch <seanwelch51199@icloud.com>
2022-12-13 14:08:30 +0000

    updated blog area on main page with jinja

--- farmec/views.py
+++ farmec/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from suppliers.models import Supplier
 from amenity.models import Amenity
+from blog.models import Blog
 
 def home(request):
     suppliers = Supplier.objects.all()
     amenitys = Amenity.objects.all()
+    blogs = Blog.objects.order_by('created')[:3]
 
-    context = {'suppliers': suppliers, 'amenitys': amenitys}
+    context = {'suppliers': suppliers, 'amenitys': amenitys, 'blogs': blogs}
     return render(request, 'home.html', context)
 
 def contact(request):
