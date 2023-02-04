0f274d1e64764fbf5a798ec19f7eed46919a82cd
sean-david-welch <seanwelch51199@icloud.com>
2023-02-04 18:22:19 +0000

    created postgres db

--- payments/views.py
+++ payments/views.py
@@ -6,6 +6,12 @@ import stripe
 # Create your views here.
 stripe.api_key = os.environ.get('TEST_SECRET_KEY')
 
+def calculate_order_amount(items):
+    # Replace this constant with a calculation of the order's amount
+    # Calculate the order total on the server to prevent
+    # people from directly manipulating the amount on the client
+    return 1400
+
 def payments(request):
     context = {}
     return render(request, 'payments/payments.html', context)
