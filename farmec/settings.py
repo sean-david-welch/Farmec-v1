afed995f7560fc2014f0e6a9a25d875feb75465e
sean-david-welch <seanwelch51199@icloud.com>
2023-03-02 19:13:22 +0000

    collected static again

--- farmec/settings.py
+++ farmec/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -35,7 +35,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
