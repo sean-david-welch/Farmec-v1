a8f501d8b8356df9a7c7588830645644ded7b277
sean-david-welch <seanwelch51199@icloud.com>
2023-03-04 15:26:31 +0000

    added all stylesheets and scripts

---
+++ staticfiles/js/footer.js
@@ -0,0 +1,8 @@
+// To top button
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY < 100) {
+        toTop.classList.remove(".ready");
+    }
+});
