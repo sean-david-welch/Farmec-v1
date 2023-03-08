3bdb0e08d745c443260cf924bfea50692a929074
sean-david-welch <seanwelch51199@icloud.com>
2023-03-08 16:49:53 +0000

    updated machine model

--- suppliers/models.py
+++ suppliers/models.py
@@ -45,7 +45,7 @@ class Machine(models.Model):
     name = models.CharField(max_length=200, blank=True, null=True)
     machine_image = models.ImageField(null=True, blank=True, upload_to='models/', default="default.jpg")
     description = models.TextField(blank=True, null=True)
-    short_description = models.CharField(max_length=200, blank=True, null=True)
+    machine_link = models.URLField(max_length=200, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
