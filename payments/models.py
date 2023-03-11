ab4652c7c3c4f47aa62cf54087f7582256e45868
sean-david-welch <seanwelch51199@icloud.com>
2023-03-11 17:19:31 +0000

    Decimal places fixed

--- payments/models.py
+++ payments/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class PaymentProduct(models.Model):
     name = models.CharField(max_length=200, blank=True, null=True)
     serialnumber = models.CharField(max_length=200, blank=True, null=True)
-    price = models.DecimalField(default=0, null=True, blank=True)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=8, null=True, blank=True)
     image = models.ImageField(upload_to='models/', null=True, blank=True)
 
     def get_price_in_cents(self):
