fc7949424feb4211dfeeaee1eda8b0ebc963c85d
sean-david-welch <seanwelch51199@icloud.com>
2023-03-11 16:50:43 +0000

    float price

--- payments/models.py
+++ payments/models.py
@@ -11,8 +11,8 @@ class PaymentProduct(models.Model):
     def __str__(self):
         return str(self.name)
 
-    def get_display_price(self):
-        return "{0:.2f}".format(self.price / 100)
+    # def get_display_price(self):
+    #     return "{0:.2f}".format(self.price / 100)
     
     @property
     def imageurl(self):
--- payments/signals.py
+++ payments/signals.py
@@ -2,6 +2,6 @@ from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from .models import PaymentProduct
 
-@receiver(pre_save, sender=PaymentProduct)
-def multiply_price(sender, instance, **kwargs):
-    instance.price *= 100
+# @receiver(pre_save, sender=PaymentProduct)
+# def multiply_price(sender, instance, **kwargs):
+#     instance.price *= 100
