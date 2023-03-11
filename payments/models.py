171899b5d3d625ab2d86ee963fc93d0482be485c
sean-david-welch <seanwelch51199@icloud.com>
2023-03-11 16:37:43 +0000

    Stripe Floatfield

--- payments/models.py
+++ payments/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 class PaymentProduct(models.Model):
     name = models.CharField(max_length=200, blank=True, null=True)
-    price = models.IntegerField(default=0, null=True, blank=True) #cents
+    serialnumber = models.CharField(max_length=200, blank=True, null=True)
+    price = models.FloatField(default=0, null=True, blank=True)
     image = models.ImageField(upload_to='models/', null=True, blank=True)
-    url = models.URLField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return str(self.name)
