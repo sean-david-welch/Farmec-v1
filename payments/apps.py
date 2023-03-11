47bd53a950dedb080e211398edee5d8b15183141
sean-david-welch <seanwelch51199@icloud.com>
2023-03-11 17:36:27 +0000

    Stripe & Youtube Apis Fixed

--- payments/apps.py
+++ payments/apps.py
@@ -5,5 +5,3 @@ class PaymentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'payments'
 
-    def ready(self):
-        import payments.signals
--- payments/signals.py
+++
@@ -1,7 +0,0 @@
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
-from .models import PaymentProduct
-
-# @receiver(pre_save, sender=PaymentProduct)
-# def multiply_price(sender, instance, **kwargs):
-#     instance.price *= 100
